04f47aa760fcc8adfb703bfc51283cf54bcdb8c3
slathas24 <48292537+slathas24@users.noreply.github.com>
2022-12-04 18:08:51 +0530

    Update Constants.js

--- smartbankfrontend/src/Constants.js
+++ smartbankfrontend/src/Constants.js
@@ -1,2 +1,5 @@
-
-export const API_URL = "http://localhost:8080/";
+let protocol="http://"
+let ip= process.env.REACT_APP_API_IP || "localhost"
+let port = process.env.REACT_APP_API_PORT || "3000";
+export const API_URL = protocol+ip+":"+port;
+//export const API_URL = "http://localhost:8080/";
